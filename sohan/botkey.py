4869b5a34d3f2bd01d7668ceaaa06cb130cd690d
Sree Hari Nagaralu <nsreehari@outlook.com>
2016-02-28 18:46:42 +0530

    msc

--- sohan/botkey.py
+++ sohan/botkey.py
@@ -30,7 +30,10 @@ if not BOTKEY:
         sys.exit()
 
 LOGFILE = "/tmp/telegram_logs/%s" % os.path.basename(BOTHANDLE)
-os.makedirs("/tmp/telegram_logs", 0755)
+try:
+    os.makedirs("/tmp/telegram_logs", 0755)
+except:
+    pass
 
 # Enable logging
 logging.basicConfig(filename=LOGFILE,
