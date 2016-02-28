d94d5e6f0b8817ccc85a3b8860c8cb644f52207d
Sanjay Prakash <sanjaya.g.prakash1992@gmail.com>
2016-02-28 09:29:23 +0000

    initial

---
+++ ctl_manager.sh
@@ -0,0 +1,123 @@
+#! /bin/sh
+
+if [ "x$2" = "x" ];  then
+    echo Error: No Bot Handle given
+    exit 
+fi
+
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+date
+
+BOTS_CONFIGS="$SCRIPTPATH/.bots"
+BOT_HANDLE="$2"
+BOT_HANDLE_DIR="$BOTS_CONFIGS/$BOT_HANDLE"
+if [ ! -d "$BOT_HANDLE_DIR" ]; then
+    echo Error: Bot Not Registered
+    exit 
+fi
+
+GEVENT_START="`cat $BOT_HANDLE_DIR/.runscript`"
+
+OPENERP_HOME=`dirname $GEVENT_START`
+GEVENT_PROGRAM=`basename $GEVENT_START`
+GEVENT_PROGRAM1=`echo $GEVENT_PROGRAM | sed -e 's/^[a-zA-Z0-9]/[&]/g'`
+STOP_FILE="$OPENERP_HOME/.stop_$BOT_HANDLE"
+
+GEVENT_STATUS=""
+GEVENT_PID=""
+GEVENT_STATUS=""
+ERROR=0
+
+is_service_running() {
+    GEVENT_PID=`ps ax | awk '/\/'"$GEVENT_PROGRAM1"'/ {print $1}'`
+    if [ "x$GEVENT_PID" != "x" ]; then
+        RUNNING=1
+    else
+        RUNNING=0
+    fi
+    return $RUNNING
+}
+
+is_gevent_running() {
+    is_service_running "$GEVENT_PROGRAM"
+    RUNNING=$?
+    if [ $RUNNING -eq 0 ]; then
+        GEVENT_STATUS="$GEVENT_PROGRAM not running"
+    else
+        GEVENT_STATUS="$GEVENT_PROGRAM already running"
+    fi
+    return $RUNNING
+}
+
+start_gevent() {
+    test -f $STOP_FILE && echo $STOP_FILE exists: exiting &&exit
+    is_gevent_running
+    RUNNING=$?
+    if [ $RUNNING -eq 1 ]; then
+        echo "$0 $ARG: $GEVENT_PROGRAM already running"
+        exit
+    fi
+    if [ `id -u` != 0 ]; then
+        /bin/sh -c "((cd $OPENERP_HOME && $GEVENT_START  $BOT_HANDLE_DIR 2>&1) >>/tmp/cron.out ) &"
+    else
+        su - daemon -s /bin/sh -c "((cd $OPENERP_HOME && $GEVENT_START   $BOT_HANDLE_DIR 2>&1) >/dev/null) &"
+    fi
+    sleep 4
+    is_gevent_running
+    RUNNING=$?
+    if [ $RUNNING -eq 0 ]; then
+        ERROR=1
+    fi
+
+    if [ $ERROR -eq 0 ]; then
+        echo "$0 $ARG: $GEVENT_PROGRAM started"
+        sleep 2
+    else
+        echo "$0 $ARG: $GEVENT_PROGRAM could not be started"
+        ERROR=3
+    fi
+}
+
+stop_gevent() {
+    NO_EXIT_ON_ERROR=$1
+    is_gevent_running
+    RUNNING=$?
+    if [ $RUNNING -eq 0 ]; then
+        echo "$0 $ARG: $GEVENT_STATUS"
+        if [ "x$NO_EXIT_ON_ERROR" != "xno_exit" ]; then
+            exit
+        else
+            return
+        fi
+    fi
+
+    kill $GEVENT_PID
+    sleep 6
+
+    is_gevent_running
+    RUNNING=$?
+    if [ $RUNNING -eq 0 ]; then
+            echo "$0 $ARG: $GEVENT_PROGRAM stopped"
+        else
+            echo "$0 $ARG: $GEVENT_PROGRAM could not be stopped"
+            ERROR=4
+    fi
+}
+
+if [ "x$1" = "xstart" ]; then
+    start_gevent
+elif [ "x$1" = "xstop" ]; then
+    stop_gevent
+elif [ "x$1" = "xrestart" ]; then
+    stop_gevent
+    sleep 3
+    start_gevent
+elif [ "x$1" = "xstatus" ]; then
+    is_gevent_running
+    echo "$GEVENT_STATUS"
+fi
+
+exit $ERROR
---
+++ telegram_mgmt.py
@@ -0,0 +1,156 @@
+#!/opt/bitnami/python/bin/python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+This Bot uses the Updater class to handle the bot.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram import Updater
+import logging
+import json, sys
+
+import os
+SCRIPTPATH = os.path.dirname(os.path.realpath(__file__))
+BOTSCONFIGDIR = SCRIPTPATH + "/.bots/"
+
+
+BOTKEY = ""
+
+if not BOTKEY:
+    try:
+        BOTHANDLE = sys.argv[1]
+        f = open('%s/.botkey' % BOTHANDLE)
+        BOTKEY = f.readline().strip()
+        f.close()
+    except:
+        pass
+
+if not BOTKEY:
+    try:
+        BOTHANDLE = SCRIPTPATH
+        f = open(SCRIPTPATH + '/.BOTKEY')
+        BOTKEY = f.readline().strip()
+        f.close()
+    except:
+        print "BOTKEY not provided"
+        sys.exit()
+
+LOGFILE = "/tmp/telegram_logs/%s" % os.path.basename(BOTHANDLE)
+
+# Enable logging
+logging.basicConfig(filename=LOGFILE,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi Hari!')
+
+from subprocess import call, check_output, STDOUT
+
+def ssrs(bot, update):
+    kw = map(lambda x: x.strip().lower(), update.message.text.strip().split())
+    CTLMANAGER = SCRIPTPATH + "/ctl_manager.sh"
+    CMD = kw[0][1:]
+    BOTHANDLE = kw[1]
+    outp = check_output([CTLMANAGER, CMD, BOTHANDLE], stderr=STDOUT) 
+    bot.sendMessage(update.message.chat_id, text=outp)
+
+def newbot(bot, update):
+    kw = map(lambda x: x.strip().lower(), update.message.text.strip().split())
+    try:
+            CMD = kw[0][1:]
+            CHANNEL = kw[1]
+            BOTHANDLE = kw[2]
+            BOTKEY = kw[3]
+            GITPATH = kw[4]
+            SCRIPT_TO_RUN = kw[5]
+    except:
+            outp = "Usage: /newbot telegram <bothandle> <botkey> <gitpath> <script_to_run>"
+            bot.sendMessage(update.message.chat_id, text=outp)
+            return
+
+    if CHANNEL not in ['telegram']:
+        outp = "Channel %s not supported" % CHANNEL
+        bot.sendMessage(update.message.chat_id, text=outp)
+        return
+
+    if CHANNEL in ['telegram']:
+        bdir = BOTSCONFIGDIR + BOTHANDLE
+        if os.path.isdir(bdir):
+            outp = "Error! Bot already exists"
+            bot.sendMessage(update.message.chat_id, text=outp)
+            return
+
+        for (f, c) in [ ('.botkey', BOTKEY),
+                        ('.runscript', SCRIPT_TO_RUN),
+                        ('.gitpath', GITPATH) ]:
+            f = open(bdir + '/' + f, "w+")
+            f.write(c)
+            f.close()
+
+        outp = "Bot %s registered!" % BOTHANDLE
+        bot.sendMessage(update.message.chat_id, text=outp)
+        return
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Help!')
+
+def echo(bot, update):
+    bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(BOTKEY)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    for cmd in ['up', 'down', 'refresh', 'status']:
+        dp.addTelegramCommandHandler(cmd, ssrs)
+
+    for cmd in ['newbot']:
+        dp.addTelegramCommandHandler(cmd, newbot)
+
+    dp.addTelegramCommandHandler("start", help)
+    dp.addTelegramCommandHandler("help", help)
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.addTelegramMessageHandler(echo)
+
+    # log all errors
+    dp.addErrorHandler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+
